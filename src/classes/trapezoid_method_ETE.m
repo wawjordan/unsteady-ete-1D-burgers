c907839ba889e50b65a33fa9d0620c2c0ad9108c
wawjordan <wajordan@vt.edu>
2021-06-28 19:37:13 -0400

    Explored some modifications involving the discrete residual of reconstructed solutions However, I don't think this is the correct approach

--- src/classes/trapezoid_method_ETE.m
+++ src/classes/trapezoid_method_ETE.m
@@ -3,6 +3,7 @@ classdef trapezoid_method_ETE < time_integrator_type
         imax, i_low, i_high, i
         tau
         Rnorm, Rinit
+        pos
     end
     methods
         function this = trapezoid_method_ETE(soln_error)
@@ -10,11 +11,14 @@ classdef trapezoid_method_ETE < time_integrator_type
             this.i_high = soln_error.i_high;
             this.imax = soln_error.imax;
             this.i = soln_error.i;
-        end  
+            this.pos = soln_error.M;
+        end
         function [e_new,R,this] = step(this,soln,soln_error,~)
 %============ use this one for asymmetric stencil     ========%
-            ind1 = soln_error.ptr(soln_error.M);
-            ind2 = soln_error.ptr(soln_error.M+1);
+%             ind1 = soln_error.ptr(soln_error.M);
+%             ind2 = soln_error.ptr(soln_error.M+1);
+            ind1 = soln_error.ptr(this.pos);
+            ind2 = soln_error.ptr(this.pos+1);
 %============ use this one for symmetric time stencil ========%
 %             ind1 = soln_error.ptr(soln_error.M/2);
 %             ind2 = soln_error.ptr(soln_error.M/2+1);
@@ -34,18 +38,35 @@ classdef trapezoid_method_ETE < time_integrator_type
             Rue2 = soln.residual(ue2);
             Ru1 = soln.residual(u1);
             Ru2 = soln.residual(u2);
-            [u01,u11,soln_error] = soln_error.time_TE_est(t1);
-            [u02,u12,soln_error] = soln_error.time_TE_est(t2);
-%             [u03,~,soln_error] = soln_error.time_TE_est((t2+t1)/2);
+            [u01,u11] = soln_error.time_TE_est(t1);
+            [u02,u12] = soln_error.time_TE_est(t2);
             [TE1,~] = soln_error.space_TE_est(soln,u01);
             [TE2,~] = soln_error.space_TE_est(soln,u02);
             TE1 = TE1 + u11(this.i);
             TE2 = TE2 + u12(this.i);
             F1 = Rue1 - Ru1 + TE1;
             F2 = Rue2 - Ru2 + TE2;
+
+            
+%             [TE1,RE1,u01] = soln_error.space_TE_est(soln,u01);
+%             [TE2,RE2,u02] = soln_error.space_TE_est(soln,u02);
+%             Tdudt1 = u11(this.i);
+%             Tdudt2 = u12(this.i);
+%             Rdudt = (u02(this.i)-u01(this.i))/soln.dt;
+%             clf;
+%             hold on;
+%             TE1 = TE1 + Tdudt1;
+%             TE2 = TE2 + Tdudt2;
+%             plot(0.5*(TE1+TE2),'b')
+%             TE1 = TE1 - (RE1+Rdudt);
+%             TE2 = TE2 - (RE2+Rdudt);
+%             F1 = Rue1 - Ru1 + TE1;
+%             F2 = Rue2 - Ru2 + TE2;
+            
+%             plot(0.5*(TE1+TE2),'r')
+%             hold off;
             
             this.tau = 0.5*(TE1+TE2);
-            
             J2 = -(1/2)*soln.dt.*J;
             J2(:,2) = J2(:,2) + 1;
             
@@ -63,10 +84,16 @@ classdef trapezoid_method_ETE < time_integrator_type
                 this.Rinit(1,k) = norm(R_new(:,k));
             end
             while (any(this.Rnorm>this.newton_tol))&&(j<this.newton_max_iter)
+                J = soln.jacobian(ue2);
+                J2 = -(1/2)*soln.dt.*J;
+                J2(:,2) = J2(:,2) + 1;
+                
                 ue2(this.i) = u2(this.i)-e_new;
                 Rue2 = soln.residual(ue2);
+                
                 F2 = Rue2 - Ru2 + TE2;
-                RHS = -(e_new-soln_error.error-0.5*soln.dt*(F1+F2));
+                RHS = 0.5*soln.dt*(F1+F2)-(e_new-soln_error.error);
+%                 RHS = -(e_new-soln_error.error-0.5*soln.dt*(F1+F2));
                 de = tridiag(J2(:,1),J2(:,2),J2(:,3),RHS);
                 e_new = e_new + de;
                 R_new = RHS - (J2(:,1).*de + J2(:,2).*de + J2(:,3).*de);
