48d5e9d232426ae3a56ff5b4eede9defe8126aca
wawjordan <wajordan@vt.edu>
2021-06-28 08:18:12 -0400

    various changes - updating remote

--- test/test_script1.m
+++ test/test_script1.m
@@ -2,7 +2,7 @@
 clc; clear; close all;
 
 bgrid = grid1D(linspace(-4,4,256),5);
-bsoln = burgers1D(bgrid,64,'TimeAccurate',true,'TimeRange',[-2,-1],...
+bsoln = burgers1D(bgrid,64,'TimeAccurate',true,'TimeRange',[-2,-1.8],...
     'dt',0.01,'ExactSolutionType','unsteady_shock');
 BC = exact_BC(bgrid);
 err_soln = burgers1D_error( bsoln,'ReconstructionOrder',4);
@@ -15,7 +15,7 @@ BD_ETE = back_diff_2_ETE(err_soln);
 maxiter = 500;
 
 
-[bsoln,err_soln,TM,TM_ETE,Primal,Error] = ETEsolver2(bsoln,err_soln,TM,TM_ETE,BC,maxiter,1);
+[bsoln,err_soln,BD,BD_ETE,Primal,Error] = ETEsolver2(bsoln,err_soln,BD,BD_ETE,BC,maxiter,1);
 
 %%
 hold on;
