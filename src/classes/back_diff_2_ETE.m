a4f7cf9b64d261217aa53b1569fad5ba04faa0e4
wawjordan <wajordan@vt.edu>
2021-06-30 14:11:39 -0400

    Added pos and u_old properties for use with iterative correction Changed indexing to use pos

--- src/classes/back_diff_2_ETE.m
+++ src/classes/back_diff_2_ETE.m
@@ -4,6 +4,7 @@ classdef back_diff_2_ETE < time_integrator_type
         tau
         Rnorm, Rinit
         em1, em2
+        pos, u_old
     end
     methods
         function this = back_diff_2_ETE(soln_error)
@@ -11,28 +12,35 @@ classdef back_diff_2_ETE < time_integrator_type
             this.i_high = soln_error.i_high;
             this.imax = soln_error.imax;
             this.i = soln_error.i;
+            this.pos = soln_error.M;
 
             this.em1 = soln_error.error;
             this.em2 = soln_error.error;
+            this.u_old = soln_error.stencil;
         end
         function [e_new,R,this] = step(this,soln,soln_error,~)
             e_new = soln_error.error;
+            ind = soln_error.ptr(this.pos+1);
+%             ind1 = soln_error.ptr(this.pos);
+%             ind2 = soln_error.ptr(this.pos-1);
 %============ use this one for asymmetric stencil     ========%
-            ind = soln_error.ptr(soln_error.M+1);
-            ind1 = soln_error.ptr(soln_error.M);
-            ind2 = soln_error.ptr(soln_error.M-1);
+%             ind = soln_error.ptr(soln_error.M+1);
+%             ind1 = soln_error.ptr(soln_error.M);
+%             ind2 = soln_error.ptr(soln_error.M-1);
+            
 %============ use this one for symmetric stencil     ========%
 %             ind = soln_error.ptr(soln_error.M/2+1);
             
-            t1 = soln_error.t(ind1);
-            t2 = soln_error.t(ind2);
+%             t1 = soln_error.t(ind1);
+%             t2 = soln_error.t(ind2);
 %             disp(t1);
 %             disp(t2);
             dt = soln.dt;
             t = soln_error.t(ind);
             
 %             u = soln.U;
-            u = soln_error.stencil(:,ind);
+%             u = soln_error.stencil(:,ind);
+            u = this.u_old(:,ind);
             
             ue = u;
             ue(this.i) = u(this.i)-e_new;
@@ -51,7 +59,8 @@ classdef back_diff_2_ETE < time_integrator_type
             [u0,u13,soln_error] = soln_error.time_TE_est(t);
 %             [~,~,u01] = soln_error.space_TE_est(soln,u01);
 %             [~,~,u02] = soln_error.space_TE_est(soln,u02);
-            [TE,RE,u0] = soln_error.space_TE_est(soln,u0);
+            [TE,~,~] = soln_error.space_TE_est(soln,u0);
+%             [TE,RE,u0] = soln_error.space_TE_est(soln,u0);
 %             [~,TE] = soln_error.space_TE_est(soln,u03);
             Tdudt = u13(soln.i);
 %             Rdudt = (3*u0(this.i) - 4*u01(this.i) + u02(this.i))/(2*dt);
@@ -75,28 +84,30 @@ classdef back_diff_2_ETE < time_integrator_type
             e_new = e_new + de;
             R_new = RHS - (J2(:,1).*de + J2(:,2).*de + J2(:,3).*de);
             j = 1;
-            if soln_error.count == 1
+%             if soln_error.count == 1
                 this.Rnorm(1,:) = 1;
                 R(1,:) = this.Rnorm;
                 for k = 1:soln.neq
                     this.Rinit(1,k) = norm(R_new(:,k),2)/(soln.grid.imax^(1/2));
                 end
-            else
-                for k = 1:soln.neq
-                    this.Rnorm(1,k) = norm(R_new(:,k),2)/(soln.grid.imax^(1/2))/this.Rinit(1,k);
-                end
-                R(1,:) = this.Rnorm;
-            end
+%             else
+%                 for k = 1:soln.neq
+%                     this.Rnorm(1,k) = norm(R_new(:,k),2)/(soln.grid.imax^(1/2))/this.Rinit(1,k);
+%                 end
+%                 R(1,:) = this.Rnorm;
+%             end
             while (any(this.Rnorm>this.newton_tol))&&(j<this.newton_max_iter)
                 ue(this.i) = u(this.i)-e_new;
                 
-%                 J = soln.jacobian(ue);
-%                 J2 = -(2/3)*dt.*J;
-%                 J2(:,2) = J2(:,2) + 1;
+                J = soln.jacobian(ue);
+                J2 = -(2/3)*dt.*J;
+                J2(:,2) = J2(:,2) + 1;
                 
                 Rue = soln.residual(ue);
-                RHS = -(e_new - (4/3)*this.em1 + (1/3)*this.em2 + ...
-                    (2/3)*dt.*(-Rue+Ru-TE));
+                RHS = -(e_new - (4/3)*this.em1 + (1/3)*this.em2 - ...
+                    (2/3)*dt.*(Rue-Ru+TE));
+%                 RHS = -(e_new - (4/3)*this.em1 + (1/3)*this.em2 + ...
+%                     (2/3)*dt.*(-Rue+Ru-TE));
                 de = tridiag(J2(:,1),J2(:,2),J2(:,3),RHS);
                 e_new = e_new + de;
                 R_new = RHS - (J2(:,1).*de + J2(:,2).*de + J2(:,3).*de);
