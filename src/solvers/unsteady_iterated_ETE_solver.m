e0862e738b368ad5c170e92ddc4d8670c431425b
wawjordan <wajordan@vt.edu>
2021-06-30 14:06:29 -0400

    Added reset to error estimate so that corrections are only applied once Also created a swap stencil for transitioning between initial ETE solve and subsequent iterative corrections iterative correction now appears to be working higher iteration counts do not appear stable for higher reconstruction orders yet (i.e. 6), but 4th order reconstruction seems ok

--- src/solvers/unsteady_iterated_ETE_solver.m
+++ src/solvers/unsteady_iterated_ETE_solver.m
@@ -1,4 +1,8 @@
 function [soln,err,integrator,ETE_integrator,Primal,Error] = unsteady_iterated_ETE_solver(soln,err,integrator,ETE_integrator,bndry_cond,max_steps,out_interval)
+num_iter = 10;
+Primal = 0;
+Error = 0;
+
 % reset ETE solution object
 err = err.reset;
 
@@ -7,6 +11,7 @@ Nds = soln.i_high - soln.i_low + 1;
 
 % local variable (time stencil length)
 stenLength = err.M + 1;
+
 
 % set solution initial conditions
 soln.t = soln.t0;
@@ -17,7 +22,8 @@ soln.U = soln.ExactSolution;
 err.error = 0*err.error;
 
 % Allocate space for time stencil (error estimates)
-updateStencil = zeros(Nds,stenLength);
+estError = zeros(Nds,stenLength);
+
 exactError = zeros(Nds,stenLength);
 
 % First value is initial condition
@@ -39,51 +45,90 @@ end
 initialStencil = err.stencil;
 
 hold on;
-plot(exactError,'k')
 
-% solve ETE for each node in stencil
+plot(exactError(:,stenLength),'k')
 for i = 1:stenLength-1
-    ETE_integrator.pos = i;
-    [err.error,~,ETE_integrator] = ETE_integrator.step(soln,err,bndry_cond);
-    updateStencil(:,i+1) = err.error;
-%     fprintf([repmat('%d ',1,err.M+1),'\n'],err.ptr);
-%     fprintf('%f %f\n',err.t(err.ptr(i)),err.t(err.ptr(i+1)));
+    plot(exactError(:,i),'k','handlevisibility','off')
 end
 
+
+% solve ETE for each solution in stencil (march forward in time)
+for i = 1:stenLength-1
+    ETE_integrator.pos = i;
+    [err.error,~,~] = ETE_integrator.step(soln,err,bndry_cond);
+    estError(:,i+1) = err.error;
+end
+tempError = err.error;
+plot(estError,'r')
 % Correct solutions in stencil
-err.stencil(soln.i,:) = err.stencil(soln.i,:) - updateStencil;
+if num_iter > 0
+    err.stencil(soln.i,:) = err.stencil(soln.i,:) - estError;
+end
 
-
-plot((initialStencil(soln.i,:)-err.stencil(soln.i,:)),'r')
-
-Niter = 100;
-
-for k = 1:Niter
+% Correct primal solution, reset error estimate, and march forward again
+for k = 1:num_iter
+  err.error = 0*err.error;
 % Solve ETE again
 for i = 1:stenLength-1
     ETE_integrator.pos = i;
-    [err.error,~,ETE_integrator] = ETE_integrator.step(soln,err,bndry_cond);
-    updateStencil(:,i+1) = err.error;
+    [err.error,~,~] = ETE_integrator.step(soln,err,bndry_cond);
+    estError(:,i+1) = err.error;
 end
 
 % Correct solutions in stencil
-err.stencil(soln.i,:) = err.stencil(soln.i,:) - updateStencil;
-% plot((initialStencil(soln.i,:)-err.stencil(soln.i,:)),'b')
+if num_iter > 0
+    err.stencil(soln.i,:) = err.stencil(soln.i,:) - estError;
 end
-plot((initialStencil(soln.i,:)-err.stencil(soln.i,:)),'b')
+
+end
+plot(initialStencil(soln.i,:)-err.stencil(soln.i,:),'b')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for j = stenLength+1:stenLength+10
+    soln.count = j-1;
+    soln.t = soln.t + soln.dt;
+    
+    % solve primal
+    [soln.U,~,integrator] = integrator.step(soln,bndry_cond);
+    soln.error = soln.U(soln.i) - soln.ExactSolution(soln.i);
+    plot(soln.error,'k')
+    
+    % update stencil
+    
+    err.stencil(:,err.ptr(1)) = soln.U;
+    initialStencil(:,err.ptr(1)) = soln.U;
+    swapStencil = err.stencil;
+    err.stencil = initialStencil;
+    
+    
+    err.t(err.ptr(1)) = soln.t;
+    err.ptr = circshift(err.ptr,-1);
+    
+    ETE_integrator.pos = err.M;
+    err.error = tempError;
+    
+    % solve ETE for new time step
+    [err.error,~,~] = ETE_integrator.step(soln,err,bndry_cond);
+    plot(err.error,'r')
+    tempError = err.error;
+    
+    err.stencil = swapStencil;
+    err.stencil(soln.i,err.ptr(err.M+1)) = err.stencil(soln.i,err.ptr(err.M+1)) - err.error;
+    
+    % Solve ETE again
+    for k = 1:num_iter
+        err.error = 0*err.error;
+        ETE_integrator.pos = err.M;
+        [err.error,~,~] = ETE_integrator.step(soln,err,bndry_cond);
+
+        % Correct solution
+        err.stencil(soln.i,err.ptr(err.M+1)) = err.stencil(soln.i,err.ptr(err.M+1)) - err.error;
+        err.stencil(:,err.ptr(err.M+1)) = bndry_cond.enforce(soln,err.stencil(:,err.ptr(err.M+1)));
+    end
+    plot(initialStencil(soln.i,err.ptr(err.M+1))-err.stencil(soln.i,err.ptr(err.M+1)),'b')
+    
+end
 hold off;
-
-
-
-% for i = stenLength+1:10
-%     soln.count = i-1;
-%     [soln.U,~,integrator] = integrator.step(soln,bndry_cond);
-%     err.stencil(:,err.ptr(1)) = soln.U;
-%     soln.t = soln.t + soln.dt;
-%     err.t(err.ptr(1)) = soln.t;
-%     err.ptr = circshift(err.ptr,-1);
-%     soln.error = soln.U(soln.i) - soln.ExactSolution(soln.i);
-% end
-
 
 end
