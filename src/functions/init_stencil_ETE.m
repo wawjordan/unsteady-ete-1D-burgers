37393f73596e0f52249dd77fc4a40d0b74c946c1
wawjordan <wajordan@vt.edu>
2021-07-06 12:35:22 -0400

    initial commit function to advance ETE for first few iterations

---
+++ src/functions/init_stencil_ETE.m
@@ -0,0 +1,25 @@
+function [soln,err,ETE_integrator,estError,Error] = init_stencil_ETE(soln,err,ETE_integrator,bndry_cond,initialStencil,Error)
+% local variable (number of nodes)
+Nds = soln.i_high - soln.i_low + 1;
+
+% local variable (time stencil length)
+stenLength = err.M + 1;
+
+estError = zeros(Nds,stenLength);
+
+% solve ETE for each solution in stencil (march forward in time)
+for i = 2:stenLength
+    err.count = i;
+    ETE_integrator.pos = i-1;
+    [err.error,resnorm,ETE_integrator] = ETE_integrator.step(soln,err,bndry_cond);
+    estError(:,i) = err.error;
+    Error.R{i,1} = resnorm;
+end
+Error.tempError = err.error;
+% Correct solutions in stencil
+err.stencil(soln.i,:) = err.stencil(soln.i,:) - estError;
+for i = 1:stenLength-1
+Error = output_error_info(Error,soln,err,initialStencil,Error.interval,i,1);
+end
+
+end
