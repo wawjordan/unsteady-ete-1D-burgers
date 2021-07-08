81137f7ea3dd698e64f2fc34690168d931b6323c
wawjordan <wajordan@vt.edu>
2021-07-08 09:48:47 -0400

    commented out assignment of swapStencil (not used in current implementation) commented out plotting commands

--- src/functions/advance_iterate_ETE.m
+++ src/functions/advance_iterate_ETE.m
@@ -1,10 +1,9 @@
 function [soln,err,ETE_integrator,Error] = advance_iterate_ETE(soln,err,ETE_integrator,bndry_cond,initialStencil,estError,Error)
 % initStencil = err.stencil;
-swapStencil = err.stencil;
+% swapStencil = err.stencil;
 for k = 1:Error.num_iter
     
 %     err.stencil = initStencil;
-    
     % solve ETE again
     err.error = Error.tempError;
     ETE_integrator.em1 = 0*estError(:,err.ptr(err.M));
@@ -16,7 +15,7 @@ for k = 1:Error.num_iter
     Error.R{err.count,k+1} = resnorm;
     Error = output_error_info(Error,soln,err,initialStencil,Error.interval,err.count,k+1);
     Error.tempError = initialStencil(soln.i,err.ptr(err.M+1))-err.stencil(soln.i,err.ptr(err.M+1));
-plot((initialStencil(soln.i,:)-err.stencil(soln.i,:)))
+% plot((initialStencil(soln.i,:)-err.stencil(soln.i,:)))
 end
-
+% plot((initialStencil(soln.i,err.ptr(err.M+1))-err.stencil(soln.i,err.ptr(err.M+1))))
 end
