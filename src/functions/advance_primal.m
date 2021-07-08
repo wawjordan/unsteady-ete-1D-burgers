698ecf795fe1687836d8e46b775ef17944471178
wawjordan <wajordan@vt.edu>
2021-07-08 09:50:30 -0400

    commented out plotting command

--- src/functions/advance_primal.m
+++ src/functions/advance_primal.m
@@ -1,9 +1,10 @@
 function [soln,err,integrator,initialStencil,Primal,Error] = advance_primal(soln,err,integrator,initialStencil,bndry_cond,Primal,Error)
 soln.count = soln.count + 1;
 soln.t = soln.t + soln.dt;
+fprintf('t = %f\n',soln.t);
 [soln.U,resnorm,integrator] = integrator.step(soln,bndry_cond);
 soln.error = soln.U(soln.i) - soln.ExactSolution(soln.i);
-plot(soln.error,'k')
+% plot(soln.error,'k')
 
 [Primal,Error] = output_primal_info(Primal,Error,soln,resnorm,Primal.interval,soln.count);
 
