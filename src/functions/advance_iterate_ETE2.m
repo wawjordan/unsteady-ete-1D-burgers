fddd115500662442de0c6291480ce5269a51c2a5
wawjordan <wajordan@vt.edu>
2021-07-13 11:02:05 -0400

    initial commit replacement for advance_iterate_ETE with alternative IC formulation

---
+++ src/functions/advance_iterate_ETE2.m
@@ -0,0 +1,33 @@
+function [soln,err,ETE_integrator,Error,STENCIL] = advance_iterate_ETE2(soln,err,ETE_integrator,bndry_cond,STENCIL,Error)
+for k = 1:Error.num_iter
+    % solve ETE again
+    % use error estimates using current iteration level
+    err.error = STENCIL(1).S(soln.i,err.ptr(err.M+1)) - STENCIL(k+2).S(soln.i,err.ptr(err.M+1));
+    ETE_integrator.em1 = STENCIL(1).S(soln.i,err.ptr(err.M)) - STENCIL(k+2).S(soln.i,err.ptr(err.M));
+    ETE_integrator.em2 = STENCIL(1).S(soln.i,err.ptr(err.M-1)) - STENCIL(k+2).S(soln.i,err.ptr(err.M-1));
+    ETE_integrator.pos = err.M;
+    
+    % reset stencil to uncorrected stencil (prevents correction from being
+    % applied twice)
+    err.stencil = STENCIL(1).S;
+    % solve ETE
+    [err.error,resnorm,~] = ETE_integrator.step(soln,err,bndry_cond);
+    
+    % correct stencil
+    STENCIL(k+2).S(soln.i,err.ptr(err.M+1)) = err.stencil(soln.i,err.ptr(err.M+1)) - err.error;
+    
+    % update err.stencil for use with "output_error_info" function
+    err.stencil(soln.i,err.ptr(err.M+1)) = err.stencil(soln.i,err.ptr(err.M+1)) - err.error;
+    
+    % save iteration info
+    Error.R{err.count,k+1} = resnorm;
+    Error = output_error_info(Error,soln,err,STENCIL(1).S,Error.interval,err.count,k+1);
+    
+    % debug/local error plots
+%     plot(err.error,'b');
+%     plot(ETE_integrator.em1,'b');
+%     plot(ETE_integrator.em2,'b');
+%     plot((STENCIL(1).S(soln.i,err.ptr(err.M+1))-STENCIL(k+2).S(soln.i,err.ptr(err.M+1))))
+end
+
+end
