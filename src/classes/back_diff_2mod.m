5c50036462b2eca50eb9aed8d00f127cc444cf4e
wawjordan <wajordan@vt.edu>
2021-10-19 14:38:22 -0400

    Commented out extrapolation BC stuff

--- src/classes/back_diff_2mod.m
+++ src/classes/back_diff_2mod.m
@@ -17,74 +17,41 @@ classdef back_diff_2mod < time_integrator_type
         end
         function [u_new,R,this] = step(this,soln,bndry_cond)
     
-            u_new = zeros(size(soln.U,1),soln.neq);
-            R = nan(this.newton_max_iter,soln.neq);
-            J = soln.jacobian(soln.U);
-            res = soln.residual(soln.U);
-            J2 = -(2/3)*soln.dt.*J;
+            u_new = soln.U;
+            R       = nan(this.newton_max_iter,soln.neq); % allocate residual norm storage
+            J       = soln.jacobian(u_new);              % calculate residual Jacobian
+            res     = -soln.residual(u_new);        % calculate steady-state residual for current solution
+            J2      = -(2/3)*soln.dt.*J;                  % modify jacobian for BDF2 algorithm
             J2(:,2) = J2(:,2) + 1;
-            RHS = -soln.U(this.i) + (4/3)*this.um1(this.i) - ...
-                (1/3)*this.um2(this.i) - (2/3)*soln.dt.*res;
+            J2(1,:) = 0;
+            J2(1,2) = 1; 
+            J2(end,:) = 0;
+            J2(end,2) = 1;
+%             tmp = J2;
+%             tmp(:,1) = circshift(J2(:,1),-1);
+%             tmp(:,3) = circshift(J2(:,3),1);
+%             J3 = spdiags(tmp,-1:1,this.imax,this.imax);
             
-            %%% Attempt at incorporating extrapolation BC
-%             a1 = -(2/3)*soln.dt*(...
-%                 soln.nu/(soln.grid.dx(this.i_low)^2) + ...
-%                 (2*soln.U(this.i_low) - soln.U(this.i_low+1))/...
-%                 (soln.grid.dx(this.i_low) + soln.grid.dx(this.i_low+1)));
-%             cN = -(2/3)*soln.dt*(...
-%                 soln.nu/(soln.grid.dx(this.i_high)^2) + ...
-%                 (2*soln.U(this.i_high) - soln.U(this.i_high-1))/...
-%                 (soln.grid.dx(this.i_high) + soln.grid.dx(this.i_high-1)));
-%             % Left boundary
-%             J2(1,2) = J2(1,2) + 2*a1;
-%             J2(1,3) = J2(1,3) - a1;
-%             % Right boundary
-%             J2(end,1) = J2(end,1) - cN;
-%             J2(end,2) = J2(end,2) + 2*cN;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%             a1 = -(2/3)*soln.dt*(...
-%                 soln.nu/(soln.grid.dx(this.i_low)^2) + ...
-%                 soln.U(this.i_low-1)/...
-%                 (soln.grid.dx(this.i_low) + soln.grid.dx(this.i_low+1)));
-%             cN = -(2/3)*soln.dt*(...
-%                 soln.nu/(soln.grid.dx(this.i_high)^2) + ...
-%                 soln.U(this.i_high+1)/...
-%                 (soln.grid.dx(this.i_high) + soln.grid.dx(this.i_high-1)));
-%             RHS(1) = RHS(1) - a1*(soln.U(this.i_low-1)  - this.um1(this.i_low-1));
-%             RHS(end) = RHS(end) - cN*(soln.U(this.i_high+1) - this.um1(this.i_high+1));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  New (as of 09/30/21)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            UEX0_0 = soln.calc_exact(soln.grid.x(this.i_low-1),soln.t);        % Exact solution @ ghost node @ current time step
-            UEX0_1 = soln.calc_exact(soln.grid.x(this.i_low-1),soln.t-soln.dt);% Exact solution @ ghost node @ previous time step
-            UEXN_0 = soln.calc_exact(soln.grid.x(this.i_high+1),soln.t);        % Exact solution @ ghost node @ current time step
-            UEXN_1 = soln.calc_exact(soln.grid.x(this.i_high+1),soln.t-soln.dt);% Exact solution @ ghost node @ previous time step
-            a1 = -(2/3)*soln.dt*( soln.nu/( soln.grid.dx(this.i_low)^2 ) + UEX0_0/(soln.grid.dx(this.i_low) + soln.grid.dx(this.i_low+1)));
-            cN = -(2/3)*soln.dt*( soln.nu/(soln.grid.dx(this.i_high)^2 ) + UEXN_0/(soln.grid.dx(this.i_high) + soln.grid.dx(this.i_high-1)));
-            RHS(1)   = RHS(1)   - a1*(UEX0_0-UEX0_1);
-            RHS(end) = RHS(end) - cN*(UEXN_0-UEXN_1);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  New (as of 09/30/21)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % F^(k+1) = u_h^(k+1) - (4/3)*u_h^n + (1/3)*u_h^(n-1) - (2/3)dt*R_h(u_h^k)
+            % RHS = -F^k
+            u1    = this.um1(this.i);
+            u1m1  = this.um2(this.i);
+            dt = soln.dt;
+            F = @(uhk) uhk - (4/3)*u1 + (1/3)*u1m1 - (2/3)*dt*res;
+            R_new = -F(u_new(this.i));
+            R_new(1) = soln.calc_exact(soln.grid.x(soln.i_low),soln.t)-u_new(soln.i_low);
+            R_new(end) = soln.calc_exact(soln.grid.x(soln.i_high),soln.t)-u_new(soln.i_high);
+            du = tridiag(J2(:,1),J2(:,2),J2(:,3),R_new);
+%             du = J3\R_new;
+            u_new(soln.i) = u_new(soln.i) + du;
             
-            
-            du = tridiag(J2(:,1),J2(:,2),J2(:,3),RHS);
-            
-            u_new(this.i) = soln.U(this.i) + du;
-            R_new = RHS - (J2(:,1).*du + J2(:,2).*du + J2(:,3).*du);
-            %%
-%             u_new = extrap_BC(soln,u_new);
-            u_new = bndry_cond.enforce(soln,u_new);                        % still necessary for current implementation
-            U1 = u_new(this.i_low-1);
-            U2 = u_new(this.i_high+1);
             j = 1;
             if this.start == true
                 this.start = false;
                 this.Rnorm(1,:) = 1;
                 R(1,:) = this.Rnorm;
                 for k = 1:soln.neq
-                    this.Rinit(1,k) = norm(R_new(:,k))/(soln.grid.imax^(1/2));
+                    this.Rinit(1,k) = norm(R_new(:),2)/(soln.grid.imax^(1/2));
                 end
             else
                 for k = 1:soln.neq
@@ -93,39 +60,23 @@ classdef back_diff_2mod < time_integrator_type
                 R(1,:) = this.Rnorm;
             end
             while (any(this.Rnorm>this.newton_tol))&&(j<this.newton_max_iter)
-                res = soln.residual(u_new);
-                J = soln.jacobian(u_new);
-                J2 = -(2/3)*soln.dt.*J;
+                
+                J       = soln.jacobian(u_new);              % calculate residual Jacobian
+                res     = -soln.residual(u_new);              % calculate steady-state residual for current solution
+                J2      = -(2/3)*soln.dt.*J;                  % modify jacobian for BDF2 algorithm
                 J2(:,2) = J2(:,2) + 1;
-                RHS = -u_new(this.i) + (4/3)*this.um1(this.i) - ...
-                (1/3)*this.um2(this.i) - (2/3)*soln.dt.*res;
-            
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  New (as of 09/30/21)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%             a1 = -(2/3)*soln.dt*( soln.nu/( soln.grid.dx(this.i_low)^2 ) + UEX0_1/(soln.grid.dx(this.i_low) + soln.grid.dx(this.i_low+1)));
-%             cN = -(2/3)*soln.dt*( soln.nu/(soln.grid.dx(this.i_high)^2 ) + UEXN_1/(soln.grid.dx(this.i_high) + soln.grid.dx(this.i_high-1)));
-%             RHS(1)   = RHS(1)   - a1*(UEX0_0-UEX0_1);
-%             RHS(end) = RHS(end) - cN*(UEXN_0-UEXN_1);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  New (as of 09/30/21)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            
-%                 u_new = bndry_cond.enforce(soln,u_new);                    % still necessary for current implementation
-%                 a1 = -(2/3)*soln.dt*(...
-%                     soln.nu/(soln.grid.dx(this.i_low)^2) + ...
-%                     u_new(this.i_low-1)/...
-%                     (soln.grid.dx(this.i_low) + soln.grid.dx(this.i_low+1)));
-%                 cN = -(2/3)*soln.dt*(...
-%                     soln.nu/(soln.grid.dx(this.i_high)^2) + ...
-%                     u_new(this.i_high+1)/...
-%                     (soln.grid.dx(this.i_high) + soln.grid.dx(this.i_high-1)));
-%                 RHS(1) = RHS(1) - a1*(soln.U(this.i_low-1)  - this.um1(this.i_low-1));
-%                 RHS(end) = RHS(end) - cN*(soln.U(this.i_high+1) - this.um1(this.i_high+1));
-
-                du = tridiag(J2(:,1),J2(:,2),J2(:,3),RHS);
+                J2(1,:) = 0;
+                J2(1,2) = 1;
+                J2(end,:) = 0;
+                J2(end,2) = 1;
+                
+                F = @(uhk) uhk - (4/3)*this.um1(this.i) + (1/3)*this.um2(this.i) - (2/3)*dt*res;
+                R_new = -F(u_new(soln.i));
+                R_new(1) = (soln.calc_exact(soln.grid.x(soln.i_low),soln.t)-u_new(soln.i_low));
+                R_new(end) = (soln.calc_exact(soln.grid.x(soln.i_high),soln.t)-u_new(soln.i_high));
+                du = tridiag(J2(:,1),J2(:,2),J2(:,3),R_new);
+                
                 u_new(this.i) = u_new(this.i) + du;
-                R_new = RHS - (J2(:,1).*du + J2(:,2).*du + J2(:,3).*du);
                 
                 j = j+1;
                 for k = 1:soln.neq
@@ -134,8 +85,31 @@ classdef back_diff_2mod < time_integrator_type
                 R(j,:) = this.Rnorm;
             end
             this.um2 = this.um1;
+
+            u_new = bndry_cond.enforce(soln,u_new);
             this.um1 = u_new;
             R = R(~isnan(R(:,1)),:);
+            
         end
+        function res = residual(~,soln,u)
+            res = 0*u(soln.i);
+            ind = (soln.i_low+1:soln.i_high-1);
+            res(2:soln.grid.imax-1) = -(soln.nu*(u(ind+1) - 2*u(ind) + ...
+               u(ind-1))./soln.grid.dx(ind).^2 - ...
+               (u(ind+1).^2 - u(ind-1).^2)./...
+               (4*soln.grid.dx(ind)));
+        end
+        function J = jacobian(~,soln,u)
+           J = zeros(soln.grid.imax,3);
+           ind1 = (soln.i_low+1:soln.i_high-1);
+           ind2 = (2:soln.grid.imax-1);
+           J(ind2,1) = soln.nu./soln.grid.dx(ind1-1).^2 + ...
+               u(ind2-1)./(2*soln.grid.dx(ind1-1));
+%            J(1,1) = 0;
+           J(ind2,2) = -2*soln.nu./soln.grid.dx(ind1).^2;
+           J(ind2,3) = soln.nu./soln.grid.dx(ind1+1).^2 - ...
+               u(ind2+1)./(2*soln.grid.dx(ind1+1));
+%            J(soln.grid.imax,3) = 0;
+       end
     end
 end
