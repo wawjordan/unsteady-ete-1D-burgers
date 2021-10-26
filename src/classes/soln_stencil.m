aa8c5edc652a6ad017ccc7b26c9831a890190eb2
wawjordan <wajordan@vt.edu>
2021-10-26 11:31:50 -0400

    Added input parser Added iteration dimension to stencil

--- src/classes/soln_stencil.m
+++ src/classes/soln_stencil.m
@@ -1,43 +1,47 @@
 classdef soln_stencil
     properties
-        M, N
+        M, N, L
         max_length
         queue_length
-        U, t
+        U, t, val
     end
     methods
         function this = soln_stencil( M, N, varargin )
             this.M = M;
             this.N = N;
-            if nargin > 3
-                error('too many input arguments')
-            end
-            if nargin > 2
-                this.max_length = max(M,varargin{1});
-            else
-                this.max_length = M;
-            end
-            this.U = zeros(N,M);
+            default_L = 1;
+            default_max_length = 10;
+            p = inputParser;
+            validPosInt = @(x) isnumeric(x) && isscalar(x) ...
+                                       && (x > 0) && mod(x,1) == 0;
+            addRequired(p,'M');
+            addRequired(p,'N');
+            addOptional(p,'L',default_L,validPosInt);
+            addOptional(p,'max_length',default_max_length,validPosInt);
+            parse(p,M,N,varargin{:});
+            this.max_length = max(M,p.Results.max_length);
+            this.L = p.Results.L;
+            this.U = zeros(N,M,this.L);
             this.t = zeros(1,M);
             this.queue_length = 1;
         end
         function this = push(this,u_new,t_new)
-            this.U(:,1:end-1) = this.U(:,2:end); % shift all columns left
+            this.U(:,1:end-1,:) = this.U(:,2:end,:); % shift left
             this.t(1:end-1) = this.t(:,2:end);
-            this.U(:,end) = u_new; % add new vector
+            this.U(:,end,:) = repmat(u_new,1,1,this.L); % add new vector
             this.t(end) = t_new;
             this.queue_length = min(this.M, this.queue_length+1);
         end
         function this = grow(this,k)
             if (this.M < this.max_length)
                 if (this.M+k < this.max_length)
-                    this.U = [zeros(this.N,k),this.U];
+                    this.U = cat(2,zeros(this.N,k,this.L),this.U);
                     this.t = [zeros(1,k),this.t];
                     this.M = this.M+k;
                 else
                     warning('specified k exceeds max_length')
                     k2 = this.max_length-this.M;
-                    this.U = [zeros(this.N,k2),this.U];
+                    this.U = cat(2,zeros(this.N,k2,this.L),this.U);
                     this.t = [zeros(1,k2),this.t];
                     this.M = this.max_length;
                 end
